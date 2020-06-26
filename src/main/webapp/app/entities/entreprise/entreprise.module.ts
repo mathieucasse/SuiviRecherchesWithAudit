9b543ca7eb49bf23e170490d5eb9b06ba546a9c1
matbreak <matbreak@jupimail.com>
2020-06-26 13:13:01 +0200

    feat: try to use dialog windows

--- src/main/webapp/app/entities/entreprise/entreprise.module.ts
+++ src/main/webapp/app/entities/entreprise/entreprise.module.ts
@@ -6,11 +6,20 @@ import { EntrepriseComponent } from './entreprise.component';
 import { EntrepriseDetailComponent } from './entreprise-detail.component';
 import { EntrepriseUpdateComponent } from './entreprise-update.component';
 import { EntrepriseDeleteDialogComponent } from './entreprise-delete-dialog.component';
+import { EntrepriseUpdateDialogComponent } from './entreprise-update-dialog.component';
+import { EntrepriseDetailDialogComponent } from './entreprise-detail-dialog.component';
 import { entrepriseRoute } from './entreprise.route';
 
 @NgModule({
   imports: [SuiviRecherchesSharedModule, RouterModule.forChild(entrepriseRoute)],
-  declarations: [EntrepriseComponent, EntrepriseDetailComponent, EntrepriseUpdateComponent, EntrepriseDeleteDialogComponent],
-  entryComponents: [EntrepriseDeleteDialogComponent]
+  declarations: [
+    EntrepriseComponent,
+    EntrepriseDetailComponent,
+    EntrepriseUpdateComponent,
+    EntrepriseDeleteDialogComponent,
+    EntrepriseUpdateDialogComponent,
+    EntrepriseDetailDialogComponent
+  ],
+  entryComponents: [EntrepriseDeleteDialogComponent, EntrepriseUpdateDialogComponent, EntrepriseDetailDialogComponent]
 })
 export class SuiviRecherchesEntrepriseModule {}
